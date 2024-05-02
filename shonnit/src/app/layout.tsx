361a4478bbbc50bc09f7b36db5495cc2759f4799
Keshon Shell <keshon178@gmail.com>
2024-05-01 21:25:36 -0400

    19:33 Timestamp

---
+++ shonnit/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Shoniit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container mx-auto max-w-7xl h-full pt-12">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
